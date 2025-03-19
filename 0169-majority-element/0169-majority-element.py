c096f20796babc3fdf12ebec1c6ed76bdaaec9f7
kunrak <125427166+kunrak@users.noreply.github.com>
2025-03-19 13:00:14 +0530

    Time: 3 ms (82.27%), Space: 19.3 MB (52.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        while True:
-            candidate = random.choice(nums)
-            if nums.count(candidate) > n // 2:
-                return candidate
+        res = count = 0
+        for num in nums:
+            if count == 0:
+                res = num
+            count += (1 if num == res else -1)
+        return res
